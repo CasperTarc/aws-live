9bd01653a3a6b8b7a57fefde50981d22fac1efc4
Yow Yee Qin <yowyq-wm22@student.tarc.edu.my>
2025-12-02 11:06:59 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cl4n8o6sndlu.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "admin_123"
+customdb = "employee"
+custombucket = "yowyeeqin-employee"
+customregion = "us-east-1"
+
